0148b62233b0e7069509151899716ab1d90e52b7
Tor Håkon <55088511+torhakon@users.noreply.github.com>
2026-04-15 13:56:41 +0200

    fix: handle stdin errors and ensure stream is resumed in readStdin function

--- src/commands/pool/prepare.ts
+++ src/commands/pool/prepare.ts
@@ -13,13 +13,15 @@ export type PoolPrepareCommandResult = {
 
 /* istanbul ignore next */
 async function readStdin(): Promise<string> {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     let data = '';
     process.stdin.setEncoding('utf8');
     process.stdin.on('data', (chunk: string) => {
       data += chunk;
     });
     process.stdin.on('end', () => resolve(data.trim()));
+    process.stdin.on('error', reject);
+    process.stdin.resume();
   });
 }
 
